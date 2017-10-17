5cd11bf1b457e592ae3df299139ef1242a4bcc3f
wangcailin <dreamcailin@hotmail.com>
2017-10-17 23:19:48 +0800

    初始化模型相关内容

---
+++ application/admin/model/Model.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+class Model extends Model
+{
+    // 表名
+    protected $name = 'model';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = false;
+
+    // 定义时间戳字段名
+    protected $createTime = false;
+    protected $updateTime = false;
+    
+    // 追加属性
+    protected $append = [
+        'addtime_text'
+    ];
+    
+
+    
+
+
+
+    public function getAddtimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : $data['addtime'];
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+    protected function setAddtimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+
+}
