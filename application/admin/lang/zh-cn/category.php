6c992507113f418934ed80e165d3196c10a76f0a
wangcailin <dreamcailin@hotmail.com>
2017-10-17 14:20:10 +0800

    初始化

---
+++ application/admin/lang/zh-cn/category.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Id'          => 'ID',
+    'Listorder'   => '排序',
+    'Pid'         => '上级栏目',
+    'Model'       => '模型',
+    'Type'        => '栏目类型',
+    'Image'       => '图片',
+    'Keywords'    => '关键字',
+    'Description' => '描述',
+    'Diyname'     => '自定义名称',
+    'Createtime'  => '创建时间',
+    'Updatetime'  => '更新时间',
+    'Status'      => '导航显示'
+];
